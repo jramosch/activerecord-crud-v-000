7aa0733466fbf04aa74b2941f8488e92da531db6
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-22 01:52:05 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {title: "The Room", release_date: 2003})
+def can_be_created_in_a_block(args = {title:nil, release_date:nil})
   Movie.create do |m|
     m.title = title
     m.release_date = release_date

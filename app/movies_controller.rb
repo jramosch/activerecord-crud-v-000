f4721b097b762e61c65927d6e0212edc317c8566
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-22 01:59:57 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -58,7 +58,7 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   awesome = Movie.create(title: "Awesome Flick")
-  awesome.udate(title: "Even Awesomer Flick")
+  awesome.update(title: "Even Awesomer Flick")
 end
 
 def can_update_using_update_method

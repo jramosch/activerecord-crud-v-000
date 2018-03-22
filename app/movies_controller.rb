164c7fce4cd2ded8f17c81805ca4be127d142a5d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-22 01:37:22 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -66,9 +66,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
-  __
+  wat = Movie.create(title: "Wat?")
+  wat.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
